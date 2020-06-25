cdfc1406d7f6add6ecb9ba705324ead3bb7d221a
Matt Braddock <matt.braddock@gmail.com>
2020-06-24 22:27:22 -0400

    testing option

--- bin/index.js
+++ bin/index.js
@@ -1,23 +1,33 @@
 #!/usr/bin/env node
 const path = require('path');
 const fs = require('fs');
+const chalk = require('chalk');
 
 const targetDir = path.join(__dirname, '../', 'lib');
 let cmd = new Map();
-let imports = fs.readdirSync(targetDir);
+let imports = fs.readdirSync(targetDir).filter(file => file.endsWith('.js'));
 imports.forEach(file => {
   const com = require(path.join(targetDir, file));
   cmd.set(com.name, com);
 });
 
 const opt = cmd.get('welcome').exe();
-switch (parseInt(opt)) {
-  case 1:
+switch (opt) {
+  case '1':
+    cmd.get('world record').exe();
     break;
-  case 2:
+  case '2':
     break;
-  case 3:
+  case '3':
     break;
-  case 4:
+  case '4':
     break;
+  case '5':
+    const pkg = require(path.join(__dirname, '../', 'package.json'))
+    console.log(chalk.yellow('Version: ' + pkg.version + '\n' + 'https://github.com/slashinfty/srcom'));
+    return;
+    break;
+  default:
+    console.log(chalk.red('Not a valid selection.'));
+    return;
 }
---
+++ readme.md
@@ -0,0 +1,19 @@
+# srcom
+
+A Node.js module that provides you with the ability to look up runs and rules from speedrun.com using the command line, in addition to posting runs.
+
+## Prerequisites
+
+[Node.js](https://nodejs.org/en/download/) installed.
+
+## Installation
+
+```
+npm i -g srcom
+```
+
+## Usage
+
+```
+srcom
+```
